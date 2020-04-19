d99fdffff494a5d6845038a39690ebf6c070a0d8
cristianpb <felipebrokate@gmail.com>
2020-04-19 19:53:16 +0200

    🐛Fix dev environement with optional basepath

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-    '/apollo',
+    dev ? '/' : '/apollo',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
