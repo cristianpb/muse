517bc79dc672b20cbc1a7f27f4a4d88faf617854
cristianpb <felipebrokate@gmail.com>
2020-05-23 11:12:25 +0200

    🎨 Handle dropdown better for browser page

--- src/routes/browse.svelte
+++ src/routes/browse.svelte
@@ -37,44 +37,42 @@
 {#if results.length > 0}
   <div class="list is-hoverable">
     {#each results as result, idx}
-      <a href={null} class="list-item" on:click={() => promise = browserUri(result, idx, 'avance')}>
-            <div class="columns is-mobile">
-              <div class="column">
-                {result.name}
+      <a href={null} class="list-item" >
+        <div class="columns is-mobile">
+          <div class="column" on:click={() => promise = browserUri(result, idx, 'avance')}>
+        {result.name}
+          </div>
+          <div class="column is-narrow" on:click={() => handleDropdownActivation(idx)}>
+            {#if options == idx}
+              <FontAwesomeIcon icon={faAngleUp} class="icon" aria-haspopup="true" aria-controls="dropdown-menu"/>
+            {:else}
+              <FontAwesomeIcon icon={faAngleDown} class="icon" aria-haspopup="true" aria-controls="dropdown-menu"/>
+            {/if}
+          </div>
+        </div>
+        {#if options == idx}
+            <div class="list is-hoverable">
+              <div class="list-item">
+                <a href="{null}" class="dropdown-item" on:click={playTrackSingle(result.uri)}>
+                  <FontAwesomeIcon icon={faPlayCircle} class="icon is-small"/>&nbsp;
+                    Play now
+                </a>
               </div>
-              <div class="column is-narrow">
-                {#if options == idx}
-                  <FontAwesomeIcon icon={faAngleUp} class="icon" aria-haspopup="true" aria-controls="dropdown-menu"/>
-                {:else}
-                  <FontAwesomeIcon icon={faAngleDown} class="icon" aria-haspopup="true" aria-controls="dropdown-menu"/>
-                {/if}
+              <div class="list-item">
+                <a href="{null}" class="dropdown-item" on:click={addTrackNext(result.uri)}>
+                  <FontAwesomeIcon icon={faArrowRight} class="icon is-small"/>&nbsp;
+                    Play next
+                </a>
+              </div>
+              <div class="list-item">
+                <a href="{null}" class="dropdown-item" on:click={addTrackQueue(result.uri)}>
+                  <FontAwesomeIcon icon={faLevelDownAlt} class="icon is-small"/>&nbsp;
+                    Add to queue
+                </a>
               </div>
             </div>
-            {#if options == idx}
-              {#if result.type == 'track'}
-                <div class="list is-hoverable">
-                  <div class="list-item">
-                    <a href="{null}" class="dropdown-item" on:click={playTrackSingle(result.uri)}>
-                      <FontAwesomeIcon icon={faPlayCircle} class="icon is-small"/>&nbsp;
-                        Play now
-                    </a>
-                  </div>
-                  <div class="list-item">
-                    <a href="{null}" class="dropdown-item" on:click={addTrackNext(result.uri)}>
-                      <FontAwesomeIcon icon={faArrowRight} class="icon is-small"/>&nbsp;
-                        Play next
-                    </a>
-                  </div>
-                  <div class="list-item">
-                    <a href="{null}" class="dropdown-item" on:click={addTrackQueue(result.uri)}>
-                      <FontAwesomeIcon icon={faLevelDownAlt} class="icon is-small"/>&nbsp;
-                        Add to queue
-                    </a>
-                  </div>
-                </div>
-              {/if}
-            {/if}
-          </a>
+        {/if}
+      </a>
     {:else}
       <a href={null} class="list-item">
         no results
@@ -116,11 +114,15 @@
         browsePath = [...browsePath, result];
       }
     } else if (result.type === 'track') {
-      if (options == idx) {
-        options = null
-      } else {
-        options = idx
-      }
+      handleDropdownActivation(idx)
+    }
+  }
+
+  const handleDropdownActivation = (idx) => {
+    if (options == idx) {
+      options = null
+    } else {
+      options = idx
     }
   }
 
