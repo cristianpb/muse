d552c7b9a85cbd68c21a7fa48db65b0bf6a08441
cristianpb <felipebrokate@gmail.com>
2020-12-22 22:47:27 +0100

    🍎 Improve support for IOS

--- rollup.config.js
+++ rollup.config.js
@@ -65,10 +65,12 @@ export default {
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
         babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
+        exclude: [ 'node_modules/@babel/**', 'node_modules/core-js/**' ],
 				presets: [
 					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
+            targets: '> 0.25%, not dead',
+            useBuiltIns: 'usage',
+            corejs: 3
 					}]
 				],
 				plugins: [
