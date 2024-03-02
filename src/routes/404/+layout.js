136be2f1eeb3a906357060309d420d06ef36e086
cristianpb <felipebrokate@gmail.com>
2024-03-02 17:16:51 +0100

    ✨ Add 404 page

--- src/routes/404/+layout.js
+++ src/routes/404/+layout.js
@@ -1,5 +1,3 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-// SPA => only index.html is prerender
-//export const prerender = true;
-//export const trailingSlash = "always";
+export const prerender = true;
